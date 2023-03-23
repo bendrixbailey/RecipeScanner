45b4db74122d75be7a8fca95804cc9cc98801a0d
bbailey1 <bbailey1@paychex.com>
2023-03-23 10:23:44 -0400

    Updated to command line based, added better options, updated readme

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # RecipeScanner
-Small program to scan a recipe webpage and assemble a json list
+Small program to scan a recipe webpage and assemble a json list for import into a document database
+
+As of now, it only works with Simplyrecipes.com and Allrecipes.com. Can easily be configured for more.
+
+# How to use
+
+The program is run via command line, and has options for either a single link, or a file with links on new lines for batch processing. You can specify a filename to write to, whether to overwrite it, or to write it to a default file named recipe.json
+
+### Usage Types
+
+```python app.py -l <link to recipe> -f <name of file> -o <name of output file> -r <if included, will overwrite output file>```
+
+For example, say you want to search one link, and put it in a file named hamburger.txt. Here is how it would work
+
+```python app.py -l www.coolrecipes.com/besthamburger -o hamburger.txt```
+
+# Planned changes/features
+
+- Moved to AWS lambda, triggered by sending an email
+- Support for taking a search result in supported website, then gathers links, then extracts the data
+- Integration with clearcook recipe website backend database
--- app.py
+++ app.py
@@ -6,71 +6,89 @@ import ssl
 import time
 import certifi
 from urllib.request import urlopen
+import argparse
 
-
-site = "SimplyRecipes"
+argparser = argparse.ArgumentParser()
 ssl._create_default_https_context = ssl._create_unverified_context
 
 def main():
-    command = ""
-    print("\
-        Website scraper for recipes. Must be customized per website.\n\
-        Enter the url of the webpage to search. If successful, a json object will be\n\
-        added to the file specified. If file is not specified/doesnt exist, a new one will\n\
-        be created.\n\
-          \n\
-        You can also supply a text file, and the program will create a recipe for every link in the text. Will skip errors.\
-        type 'exit' to quit.\n\
-        ")
-    filename = input("Enter name for file to save data to. Will default to recipe.json if no name is provided: ")
-    if(filename == ""):
-        filename = "recipe"
+    link = ""
+    infile = ""
+    outfile = ""
+    replaceOutput = False
 
-    while(command != "exit"):
-        command = input("Webpage Url> ")
-        result = ""
-        if(command != "exit" or command != "quit"):
-            if(command == "test"):
-                #webdata = requests.get("https://www.simplyrecipes.com/lemony-baked-cod-with-wild-rice-and-fennel-recipe-5224339", verify=False)
-                webdata = urlopen("https://www.simplyrecipes.com/lemony-baked-cod-with-wild-rice-and-fennel-recipe-5224339").read()
+    ingroup = argparser.add_mutually_exclusive_group()
+    ingroup.add_argument("-l", "--link", help="link to recipe webpage")
+    ingroup.add_argument("-f", "--file", help="Name of file with links to process")
+    argparser.add_argument("-o", "--out", "-output", help="name of file to output to")
+    argparser.add_argument("-r", "--replace", help="If included, will overwrite output file", action="store_true")
+    arguments = argparser.parse_args()
+
+    link = arguments.link
+    infile = arguments.file
+    outfile = arguments.out
+    replaceOutput = arguments.replace
+
+    #print(link + "-" + infile + "-" + outfile + "-" + replaceOutput)
+    #print(replaceOutput)
+
+    if(outfile == None):
+        outfile = "recipe"
+
+    result = ""
+    if(link != None):
+        if(link == "test"):
+            webdata = urlopen("https://www.simplyrecipes.com/lemony-baked-cod-with-wild-rice-and-fennel-recipe-5224339").read()
+            result = scrapeWebsite(webdata, simplyrecipes)
+        else:
+            webdata = urlopen(link).read()
+            if("simplyrecipes" in link):
                 result = scrapeWebsite(webdata, simplyrecipes)
-            elif(".txt" in command):
-                #how to open a file and read line by line python?
-                result = []
-                numlinks = 0
-                lineNum = 0
-                with open(command) as file:
-                    numlinks = len(file.readlines())
-                    file.seek(0)
-                    printProgressBar(0, numlinks, prefix="Progress", suffix="Converted", length=50)
-                    for line in file:
-                        webdata = urlopen(line).read()
-                        tempLine = ""
-                        if("simplyrecipes" in line):
-                            tempLine = scrapeWebsite(webdata, simplyrecipes)
-                        if("allrecipes" in line):
-                            tempLine = scrapeWebsite(webdata, allrecipes) 
-                        if(result != ""):
-                            result.append(tempLine)
-                        printProgressBar(lineNum + 1, numlinks, prefix="Progress", suffix="Converted", length=50)
-                        lineNum = lineNum + 1
+            if("allrecipes" in link):
+                result = scrapeWebsite(webdata, allrecipes) 
 
-            else:
-                #webdata = requests.get(command)
-                start = time.time()
-                webdata = urlopen(command).read()
-                if("simplyrecipes" in command):
-                    result = scrapeWebsite(webdata, simplyrecipes)
-                if("allrecipes" in command):
-                    result = scrapeWebsite(webdata, allrecipes) 
-                end = time.time()
-                print(end-start)
-        
-            with open(filename + ".json", "w") as outfile:
-                json.dump(result, outfile, indent=4)
-            print("Data saved to " + filename + ".json")
+    if(infile != None):
+        #how to open a file and read line by line python?
+        result = []
+        numlinks = 0
+        lineNum = 0
+        with open(infile) as file:
+            numlinks = len(file.readlines())
+            file.seek(0)
+            printProgressBar(0, numlinks, prefix="Progress", suffix="Converted", length=50)
+            for line in file:
+                webdata = urlopen(line).read()
+                tempLine = ""
+                if("simplyrecipes" in line):
+                    tempLine = scrapeWebsite(webdata, simplyrecipes)
+                if("allrecipes" in line):
+                    tempLine = scrapeWebsite(webdata, allrecipes) 
+                if(result != ""):
+                    result.append(tempLine)
+                printProgressBar(lineNum + 1, numlinks, prefix="Progress", suffix="Converted", length=50)
+                lineNum = lineNum + 1
 
-            summarizeDataPrintMissingInfo(filename + ".json")
+    if(replaceOutput):
+        with open(outfile + ".json", "w") as outputfile:
+            json.dump(result, outputfile, indent=4)
+    else:
+        with open(outfile + ".json") as inputfile:
+            try:
+                data = json.load(inputfile)
+                #print(data)
+                for item in result:
+                    #print(item)
+                    data.append(item)
+                result = data
+            except json.decoder.JSONDecodeError:
+                pass
+
+        with open(outfile + ".json", "w") as outputfile:
+            json.dump(result, outputfile, indent=4)
+    
+    print("Data saved to " + outfile + ".json")
+
+    summarizeDataPrintMissingInfo(outfile + ".json")
             
         
 
